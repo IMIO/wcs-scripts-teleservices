bed59ef6a1fa12f6f9e73be4e15be05973cdd314
Christophe Boulanger <christophe.boulanger@imio.be>
2020-01-24 14:46:22 +0100

    blackened

--- chaumongistoux_casier_judiciaire_total.py
+++ chaumongistoux_casier_judiciaire_total.py
@@ -1,2 +1,10 @@
 from decimal import Decimal
-result = str(Decimal(globals().get('form_option_cout_d_une_demande')) * int(globals().get('form_var_nb_exemplaire'))) if globals().get('form_var_objectif_raw') == "autre"  else "0"
+
+result = (
+    str(
+        Decimal(globals().get("form_option_cout_d_une_demande"))
+        * int(globals().get("form_var_nb_exemplaire"))
+    )
+    if globals().get("form_var_objectif_raw") == "autre"
+    else "0"
+)
