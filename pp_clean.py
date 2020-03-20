3daed060b90d6081cda8ec6870088925882cf8a6
Christophe Boulanger <christophe.boulanger@imio.be>
2020-03-20 15:32:21 +0100

    Portail parent : update pp_clean sample.

--- pp_clean.py
+++ pp_clean.py
@@ -1,4 +1,4 @@
-# sudo -u wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=montsaintguibert-formulaires.guichet-citoyen.be pp_get_parent_plaines_reservations .py
+# sudo -u wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=montsaintguibert-formulaires.guichet-citoyen.be pp_clean.py
 # Clean all forms in Treatment!! (Not in kart)
 from wcs.formdef import FormDef
 for formdef in FormDef.select(lambda x: x.url_name=="aes-inscrire-mon-enfant-a-une-plaine"):
