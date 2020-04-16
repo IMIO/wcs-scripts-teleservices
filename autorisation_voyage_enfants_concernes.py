f38519023cdfd81acf2344da46648ef790cf1d86
Christophe Boulanger <christophe.boulanger@imio.be>
2020-04-16 15:17:48 +0200

    Some fix.

--- autorisation_voyage_enfants_concernes.py
+++ autorisation_voyage_enfants_concernes.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 lst_enfants_concernes = globals().get("form_var_tab_enfants_concernes")
 enfants_concernes = ""
-for e in lst_enfants_concernes:
-    enfants_concernes = (
-        enfants_concernes + "-  " + e[0] + " né(e) à " + e[1] + " le " + e[2] + "\r\n"
-    )
+if lst_enfants_concernes is not None:
+    for e in lst_enfants_concernes:
+        enfants_concernes = (
+            enfants_concernes + "-  " + e[0] + " né(e) à " + e[1] + " le " + e[2] + "\r\n"
+        )
 result = enfants_concernes
