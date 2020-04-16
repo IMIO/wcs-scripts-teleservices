e23847ceb42340712be521b1a7e43a41d90472b3
Christophe Boulanger <christophe.boulanger@imio.be>
2020-04-16 15:39:23 +0200

    add little fix.

--- pp_get_encoded_diseases.py
+++ pp_get_encoded_diseases.py
@@ -39,7 +39,7 @@ def get_disease_from_index (data_source, diseases, other_diseases, index):
     return result
  
 
-if vars().get('args')[0] == "get_disease_from_index":
+if "args" in vars() and vars().get('args')[0] == "get_disease_from_index":
     if len(args) == 4:
         result = get_disease_from_index(data_source.maladies, vars().get(args[1]), vars().get(args[2]), vars().get('args')[3])
     else:
