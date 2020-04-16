54e32f0a488562d337f09736a31683fb8f008d4b
Christophe Boulanger <christophe.boulanger@imio.be>
2020-04-16 15:28:49 +0200

    Python3 : The reload built-in function has been moved to importlib module in Python 3.4

--- dest_ligne2.py
+++ dest_ligne2.py
@@ -1,4 +1,9 @@
 import sys
+try:
+    #python3.4
+    from importlib import reload
+except:
+    pass
 
 sys.path.insert(0, "/var/lib/wcs/scripts")
 sys.path.insert(0, "/var/lib/wcs-au-quotidien/scripts")
