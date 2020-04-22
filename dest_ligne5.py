c63b5e167be04087bb9e12a8d7d59a5319bd0980
Daniel Muyshond <33881615+dmshd@users.noreply.github.com>
2020-04-22 15:31:10 +0200

    fix reload is not defined

--- dest_ligne5.py
+++ dest_ligne5.py
@@ -1,8 +1,10 @@
 import sys
-
-sys.path.insert(0, "/var/lib/wcs/scripts")
-sys.path.insert(0, "/var/lib/wcs-au-quotidien/scripts")
+try:
+    from importlib import reload
+except ImportError:
+    pass
+sys.path.insert(0, '/var/lib/wcs/scripts')
+sys.path.insert(0, '/var/lib/wcs-au-quotidien/scripts')
 import dest
-
 reload(dest)
-result = "%(pays)s" % dest.get(globals())
+result = '%(pays)s' % dest.get(globals())
