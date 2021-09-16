305ed16284392c88429f3465efbc7a48aa423163
Nicolas Selva <nicolas.selva@imio.be>
2021-09-16 16:11:27 +0200

    [TELE-1087]add script to sort type of date without number of person

---
+++ liste_type_general_rdv.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import re
+
+
+def tri_rendez_vous(liste):
+    """
+    :param liste: liste json des types de rendez-vous avec le nombre de personnes encodé de la façon' - 1 personne' + pluriel ou ' pour 1 personne' + pluriel
+    :return: liste du type de rendez-vous sans le nombre de personnes
+    """
+    
+    return [re.sub(r" (pour|-) [0-9]+ personne(s)?$", "", x) for x in liste["data"]]
+
+
+try:
+    headers = {'Accept': 'application/json'}
+
+    # args[0] == l'url des types de rendez-vous
+    liste = requests.get(args[0], headers=headers).json()
+
+    result = tri_rendez_vous(liste)
+
+except Exception as e:
+    None
