151db50f61a687e5e83afba4b3ab120f61c023ee
Nicolas Selva <nicolas.selva@imio.be>
2022-07-28 14:16:50 +0200

    [TELE-1395]delete pp_clean.py

--- wcs_scripts_teleservices/pp_clean.py
+++
@@ -1,5 +0,0 @@
-# sudo -u wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=montsaintguibert-formulaires.guichet-citoyen.be pp_clean.py
-# Clean all forms in Treatment!! (Not in kart)
-from wcs.formdef import FormDef
-for formdef in FormDef.select(lambda x: x.url_name=="aes-inscrire-mon-enfant-a-une-plaine"):
-        formdef.data_class().wipe()
